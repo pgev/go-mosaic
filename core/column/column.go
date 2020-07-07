c7abf2c3316c4e60a4a90926ef6aaf73c08cf7f5
Benjamin Bollen <benjamin.bollen@gmail.com>
2020-07-07 10:26:09 +0200

    mosaicd, core: initial column

---
+++ core/column/column.go
@@ -0,0 +1,28 @@
+package column
+
+import (
+	"context"
+)
+
+// Column is the central coordination object for members of a column
+//   1. to publish their subjective view on the input batches from the past users,
+//   2. to find an objective view among the members (a threshold of signatures from members),
+//   3. to perform a computational task set by the gate (on the objective view of the input), and
+//   4. to output a batch with the resulting key:value pairs
+type Column struct {
+
+	// members map[]
+}
+
+func NewColumn(ctx context.Context) (*Column, error) {
+
+	return newColumn()
+}
+
+func NewColumnFromAddress() (*Column, error) {
+
+}
+
+func newColumn() (*Column, error) {
+
+}
---
+++ types/global_address.go
@@ -0,0 +1,9 @@
+package types
+
+// Global addresses are self-describing addresses identifying the chain
+// where the address is defined.
+
+const (
+	// Ecosystem constants
+	ECOSYSTEM_ETHEREUM = 0x01
+)
