1c9c550e04107a521bfeebc9d19ed8598201062d
Benjamin Bollen <benjamin.bollen@gmail.com>
2020-07-07 11:18:42 +0200

    core, types: minor discussions

--- core/column/column.go
+++ core/column/column.go
@@ -2,6 +2,8 @@ package column
 
 import (
 	"context"
+
+	"github.com/mosaicdao/go-mosaic/types"
 )
 
 // Column is the central coordination object for members of a column
@@ -10,8 +12,7 @@ import (
 //   3. to perform a computational task set by the gate (on the objective view of the input), and
 //   4. to output a batch with the resulting key:value pairs
 type Column struct {
-
-	// members map[]
+	members map[types.Address]*Member
 }
 
 func NewColumn(ctx context.Context) (*Column, error) {
--- types/global_address.go
+++ types/global_address.go
@@ -2,8 +2,26 @@ package types
 
 // Global addresses are self-describing addresses identifying the chain
 // where the address is defined.
-
+// <ecosystem type><varuint chainidlength ><varuint address length><chainId><address>
 const (
 	// Ecosystem constants
-	ECOSYSTEM_ETHEREUM = 0x01
+	ETHEREUM = 0x01
+	COSMOS =
+	TEZOS
 )
+
+var Names = map[string]uint64{
+	"Ethereum":	ETHEREUM,
+}
+
+var Codes = mapp[uint64]string{
+	ETHEREUM: "Ethereum",
+}
+
+var DefaultChainIdLengths = map[uint64]int{
+	ETHEREUM: 256,
+}
+
+var DefaultAddressLengths = map[uint64]int{
+	ETHEREUM: 160,
+}
