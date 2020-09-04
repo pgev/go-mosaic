ce42a965723fa6d8fc4b5a7669b9cf69ac27d9c3
Paruyr Gevorgyan <paruyr@ost.com>
2020-09-04 14:12:49 +0200

    Adds configuration files for webclient react project

---
+++ webclient/webpack.config.js
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  target: 'web',
+
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      { test: /\.js$/, enforce: 'pre', use: ['source-map-loader'] },
+      { test: /\.(js|ts)x?$/, exclude: /node_modules/, use: 'babel-loader' },
+    ],
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'www'),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+    }),
+  ],
+
+  devtool: 'source-map',
+
+  optimization: {
+    minimize: true,
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'www'),
+    compress: true,
+    port: 9000,
+  },
+};
